6f5fe15a89787f0558347550ce6f21dab17ac29a
Cuong Tran <cuongtranctc@gmail.com>
2016-11-28 12:57:54 +0000

    Added contact model file

--- db/migrate/20161127172036_create_contacts.rb
+++ db/migrate/20161127172036_create_contacts.rb
@@ -1,8 +1,8 @@
 class CreateContacts < ActiveRecord::Migration[5.0]
   def change
     create_table :contacts do |t|
-      t.strings :name
-      t.strings :email
+      t.string :name
+      t.string :email
       t.text :comments
       t.timestamps
     end

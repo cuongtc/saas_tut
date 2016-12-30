f293afe843f21c5852c9b8f62e6feb49316e45ce
Cuong Tran <cuongtranctc@gmail.com>
2016-12-30 14:09:53 +0000

    Change flash notice css fix error in registration controllers file

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -21,7 +21,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     def select_plan
       unless (params[:plan] == '1' || params[:plan] == '2')
         flash[:notice] = "Please select a membership plan to sign up."
-        redirect_to_root_url
+        redirect_to root_url
       end
     end
 end
